9aa90ac01ab7acbf95cad2b0d3f33890cb96b3ee
inspiron <inspiron@linux-5l1k.suse>
2018-01-15 20:31:12 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,16 @@
+mkdir ~/Mail
+# for maildir
+mkdir ~/Mail/cur
+mkdir ~/Mail/new
+mkdir ~/Mail/tmp
+# for mh
+mkdir ~/Mail/Inbox
+mkdir ~/Mail/Draft
+mkdir ~/Mail/Sent
+touch ~/Mail/Inbox/.mh_sequences
+touch ~/Mail/Draft/.mh_sequences
+touch ~/Mail/Sent/.mh_sequences
+# for mbox
+touch ~/Mail/inbox
+touch ~/Mail/draft
+touch ~/Mail/sent
